61bf91e8615488ef537bc68712252c4a17e77775
NourMallak <nsm32004@gmail.com>
2024-06-22 00:47:04 +0300

    second commet

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var data="";
+var title="";
+title += "<h2>My Favourite Programming Languages :</h2>";
 data += "<li>Java</li>";
 data +="<li>Python</li>";
 data += "<li>C++</li>";
-
+document.querySelector("h2").innerHTML=title;
 document.querySelector("ul").innerHTML=data;
